0159dbf60f0b20230c68259b6539db345628233c
Angelo <angelo.tiquio@outlook.com>
2025-07-31 17:06:04 -0400

    added Create contact endpoint

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ contacts = [{'id': '1','name': 'Shaun', 'phone': '123-456-7890'},
            {'id': '3', 'name': 'John', 'phone': '555-555-5555'},
            {'id': '4', 'name': 'Alice', 'phone': '444-444-4444'}]
 
+next_id = 5
+
 @app.get('/contacts')
 def list_contacts():
   return {'contacts': contacts}
@@ -22,8 +24,17 @@ def read_single_contact(id):
 
 @app.post('/contacts')
 def create_contact():
-  print(request.json)
-  return 'Thank you for the data!'
+  global next_id
+
+  new_contact = {
+    'id': f'{next_id}',
+    'name': request.json['name'],
+    'phone': request.json['phone']
+  }
+
+  next_id += 1
+  contacts.append(new_contact)
+  return new_contact, 201
 
 #GET /contacts - list all contacts
 #GET /contacts/<id> - read a single contact by id
